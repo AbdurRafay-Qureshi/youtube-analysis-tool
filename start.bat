a5bdbfd92ba47c19bf68c45599b6f660f39f8449
AbdurRafay Qureshi <163305507+AbdurRafay-Qureshi@users.noreply.github.com>
2025-10-15 19:40:32 +0500

    Enhance start.bat
    update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,25 @@
 @echo off
-echo Installing required packages, this might take a moment...
+TITLE YouTube Analysis Tool Launcher
+
+echo =========================================================
+echo  Installing required packages. This might take a moment...
+echo =========================================================
 pip install -r requirements.txt
 
+cls
+echo =========================================================
+echo  Launching the YouTube Analysis Tool...
+echo =========================================================
 echo.
-echo Launching the YouTube Analysis Tool...
+echo  Your browser should open automatically.
+echo  If it doesn't, open it and go to one of the URLs below.
+echo.
+
 streamlit run main.py
+
+echo.
+echo =========================================================
+echo  The application has been stopped.
+echo =========================================================
+pause
+
